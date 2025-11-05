032ba8f6da17967c70f3e603ff1a3c6a756baa67
Mathéo Galuba <matheo.galu56@gmail.com>
2025-11-05 09:51:18 +0100

    Fix tests

--- src/main/java/com/regifted/app/service/UserService.java
+++ src/main/java/com/regifted/app/service/UserService.java
@@ -13,48 +13,52 @@ import org.springframework.util.Assert;
 @Service
 public class UserService {
 
-    private final UserRepository repo;
-    private final BCryptPasswordEncoder passwordEncoder;
-    private final JwtUtils jwtUtils;
+  private final UserRepository repository;
+  private final BCryptPasswordEncoder passwordEncoder;
+  private final JwtUtils jwtUtils;
 
-    public UserService(UserRepository repo, BCryptPasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
-        this.repo = repo;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtils = jwtUtils;
-    }
+  public UserService(UserRepository repo, BCryptPasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
+    this.repository = repo;
+    this.passwordEncoder = passwordEncoder;
+    this.jwtUtils = jwtUtils;
+  }
 
-    public LoginResponse register(RegisterRequest req) {
-        // Check if request validity
-        Assert.hasText(req.getName(), "Name is required");
-        Assert.hasText(req.getEmail(), "Email is required");
-        Assert.isTrue(req.getEmail().matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"), "Invalid email format");
-        Assert.hasText(req.getPassword(), "Password is required");
-        // Check if email already exists
-        if (repo.existsByEmail(req.getEmail())) {
-            throw new IllegalArgumentException("Email already in use");
-        }
-        User u = new User();
-        u.setName(req.getName());
-        u.setEmail(req.getEmail());
-        u.setPassword(passwordEncoder.encode(req.getPassword()));
-        repo.save(u);
-        String token = jwtUtils.generateToken(u.getUuid(), u.getEmail());
-        return new LoginResponse(token);
+  public LoginResponse register(RegisterRequest req) {
+    // Check if request validity
+    Assert.hasText(req.getName(), "Name is required");
+    Assert.hasText(req.getEmail(), "Email is required");
+    Assert.isTrue(
+        req.getEmail().matches(
+            "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"),
+        "Invalid email format");
+    Assert.hasText(req.getPassword(), "Password is required");
+    // Check if email already exists
+    if (repository.existsByEmail(req.getEmail())) {
+      throw new IllegalArgumentException("Email already in use");
     }
+    User u = new User();
+    u.setName(req.getName());
+    u.setEmail(req.getEmail());
+    u.setPassword(passwordEncoder.encode(req.getPassword()));
+    repository.save(u);
+    String token = jwtUtils.generateToken(u.getUuid(), u.getEmail());
+    return new LoginResponse(token);
+  }
 
-    public LoginResponse login(LoginRequest req) {
-        // Check if request validity
-        Assert.hasText(req.getEmail(), "Email is required");
-        Assert.hasText(req.getPassword(), "Password is required");
-        User u = repo.findByEmail(req.getEmail()).orElseThrow(() -> new IllegalArgumentException("Invalid credentials"));
-        if (!passwordEncoder.matches(req.getPassword(), u.getPassword())) {
-            throw new IllegalArgumentException("Invalid credentials");
-        }
-        String token = jwtUtils.generateToken(u.getUuid(), u.getEmail());
-        return new LoginResponse(token);
+  public LoginResponse login(LoginRequest req) {
+    // Check if request validity
+    Assert.hasText(req.getEmail(), "Email is required");
+    Assert.hasText(req.getPassword(), "Password is required");
+    User u = repository.findByEmail(req.getEmail())
+        .orElseThrow(() -> new IllegalArgumentException("Invalid credentials"));
+    if (!passwordEncoder.matches(req.getPassword(), u.getPassword())) {
+      throw new IllegalArgumentException("Invalid credentials");
     }
+    String token = jwtUtils.generateToken(u.getUuid(), u.getEmail());
+    return new LoginResponse(token);
+  }
 
-    public User getByUuid(String uuid) {
-        return repo.findById(uuid).orElse(null);
-    }
+  public User getByUuid(String uuid) {
+    return repository.findById(uuid).orElse(null);
+  }
 }
--- src/test/java/com/regifted/app/AppApplicationTests.java
+++ src/test/java/com/regifted/app/AppApplicationTests.java
@@ -3,11 +3,10 @@ package com.regifted.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
